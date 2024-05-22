50cd115e9e9b71a411ccbab08701025e7b298675
DorAttard <104999929+DorAttard@users.noreply.github.com>
2024-05-22 20:00:46 +0200

    update

--- java/navbarScroll.js
+++ java/navbarScroll.js
@@ -1,19 +1,18 @@
 window.addEventListener('scroll', function() {
     var navbar = document.querySelector('.navbar');
     var contactSection = document.querySelector('.catalogue');
-    var classImageSection = document.querySelector('.classImage'); // Get the .classImage section
+ //   var classImageSection = document.querySelector('.classImage'); 
     var logoSVG = document.getElementById('logo');
-    var bmenu = document.getElementById('bmenu'); // Get the bmenu element
+    var bmenu = document.getElementById('bmenu'); 
   
     var contactSectionTop = contactSection.offsetTop;
     var contactSectionHeight = contactSection.offsetHeight;
-    var classImageSectionTop = classImageSection.offsetTop;
-    var classImageSectionHeight = classImageSection.offsetHeight;
+ //   var classImageSectionTop = classImageSection.offsetTop;
+//    var classImageSectionHeight = classImageSection.offsetHeight;
     var scrollPosition = window.scrollY;
 
     // Check if scroll position is within the bounds of contactSection or classImageSection
-    if ((scrollPosition >= contactSectionTop && scrollPosition < contactSectionTop + contactSectionHeight) || 
-        (scrollPosition >= classImageSectionTop && scrollPosition < classImageSectionTop + classImageSectionHeight)) {
+    if ((scrollPosition >= contactSectionTop && scrollPosition < contactSectionTop + contactSectionHeight)) {
         navbar.style.color = '#FFFBEA'; // Change text color if needed
         logoSVG.src = 'assets/logo-w.png'; // Change the image file for the SVG
         bmenu.classList.add('white-bmenu'); // Add white-bmenu class
@@ -24,3 +23,4 @@ window.addEventListener('scroll', function() {
     }
 });
 
+// ||  (scrollPosition >= classImageSectionTop && scrollPosition < classImageSectionTop + classImageSectionHeight)
